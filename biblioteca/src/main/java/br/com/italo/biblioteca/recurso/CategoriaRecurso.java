b8eb964b109227b5fb65ea0ae5adea3ac3a55f3c
italoneves <neves.italo@gmail.com>
2021-06-16 00:06:05 -0300

    Correção em Exception de atualizarCategoria

--- biblioteca/src/main/java/br/com/italo/biblioteca/recurso/CategoriaRecurso.java
+++ biblioteca/src/main/java/br/com/italo/biblioteca/recurso/CategoriaRecurso.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,13 +32,12 @@ public class CategoriaRecurso {
     public ResponseEntity<Categoria> adicionarCategoria(@RequestBody Categoria categoria){
 
         Categoria categoria1 = categoriaServico.adicionarCategoria(categoria);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(categoria1);
     }
+    @PutMapping(value = "/{id}")
     public ResponseEntity<Categoria> atualizarCategoria(@PathVariable Integer id, @RequestBody Categoria categoria){
 
         Categoria categoria1 = categoriaServico.atualizarCategoria(id, categoria);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(categoria1);
 
     }
