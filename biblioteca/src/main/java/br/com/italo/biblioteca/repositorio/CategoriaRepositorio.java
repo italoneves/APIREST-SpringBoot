0e1a2e49d24533155c63fbc46d539f2245c08af7
italoneves <neves.italo@gmail.com>
2021-06-26 18:58:00 -0300

    Implementações de consultas Titulo e Nome

--- biblioteca/src/main/java/br/com/italo/biblioteca/repositorio/CategoriaRepositorio.java
+++ biblioteca/src/main/java/br/com/italo/biblioteca/repositorio/CategoriaRepositorio.java
@@ -3,7 +3,11 @@ package br.com.italo.biblioteca.repositorio;
 import br.com.italo.biblioteca.dominio.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoriaRepositorio extends JpaRepository<Categoria, Integer> {
 
+    Optional<Categoria> findByNome(String nome);
 
 }
