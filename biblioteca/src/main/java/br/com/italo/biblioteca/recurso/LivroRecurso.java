04fdaf68b99348f931ce8983d9cdcfcb418016c4
italoneves <neves.italo@gmail.com>
2021-06-30 18:21:47 -0300

    Validação dos campos e correções no requestbody de livro

--- biblioteca/src/main/java/br/com/italo/biblioteca/recurso/LivroRecurso.java
+++ biblioteca/src/main/java/br/com/italo/biblioteca/recurso/LivroRecurso.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping(value = "/livro")
 public class LivroRecurso {
@@ -23,7 +25,7 @@ public class LivroRecurso {
     @Autowired
     private LivroServico livroServico;
 
-    @GetMapping(value = "{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Livro> buscarId(@PathVariable Integer id ){
         Livro l = livroServico.buscarId(id);
         return ResponseEntity.status(HttpStatus.OK).body(l);
@@ -31,21 +33,21 @@ public class LivroRecurso {
 
     @PostMapping
     public ResponseEntity<Livro> adicionarLivro(@RequestParam(value = "categoria",defaultValue = "0")
-                                                            Integer cat_id, @RequestBody Livro livro){
+                                                            Integer cat_id,@Valid @RequestBody Livro livro){
         Livro l = livroServico.adicionarLivro(livro,cat_id);
         return ResponseEntity.status(HttpStatus.CREATED).body(l);
     }
-    @PutMapping
-    public ResponseEntity<Livro> atualizarLivro(@PathVariable Integer id, Livro livro){
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Livro> atualizarLivro(@PathVariable Integer id,@Valid @RequestBody Livro livro){
         Livro l = livroServico.atualizarLivro(id, livro);
         return ResponseEntity.status(HttpStatus.OK).body(l);
     }
-    @PatchMapping
-    public ResponseEntity<Livro> atualizarLivroParcial(@PathVariable Integer id, Livro livro){
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Livro> atualizarLivroParcial(@PathVariable Integer id,@Valid @RequestBody Livro livro){
         Livro l = livroServico.atualizarLivro(id, livro);
         return ResponseEntity.status(HttpStatus.OK).body(l);
     }
-    @DeleteMapping(value = "{id}")
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deletarLivro(@PathVariable Integer id){
         livroServico.deletarLivro(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
