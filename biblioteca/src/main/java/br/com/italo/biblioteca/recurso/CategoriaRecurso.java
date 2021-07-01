0b99065dafaa3c71e0c664bbc8c4f7e7e8219731
italoneves <neves.italo@gmail.com>
2021-06-30 21:35:45 -0300

    CrossOrigin para permitir consumo da API

--- biblioteca/src/main/java/br/com/italo/biblioteca/recurso/CategoriaRecurso.java
+++ biblioteca/src/main/java/br/com/italo/biblioteca/recurso/CategoriaRecurso.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/categoria") //Criação de Endpoint
 public class CategoriaRecurso {
--- biblioteca/src/main/java/br/com/italo/biblioteca/recurso/LivroRecurso.java
+++ biblioteca/src/main/java/br/com/italo/biblioteca/recurso/LivroRecurso.java
@@ -5,6 +5,7 @@ import br.com.italo.biblioteca.servico.LivroServico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/livro")
 public class LivroRecurso {
