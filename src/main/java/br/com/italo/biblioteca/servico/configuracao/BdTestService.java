b5ae277eec61ba8372f40aacaa8ffea98fa302f5
italoneves <neves.italo@gmail.com>
2021-07-04 15:24:18 -0300

    Alterações na persistência do db

--- src/main/java/br/com/italo/biblioteca/servico/configuracao/BdTestService.java
+++ src/main/java/br/com/italo/biblioteca/servico/configuracao/BdTestService.java
@@ -20,14 +20,14 @@ public class BdTestService {
 
     public void popularDados(){
 
-        Categoria c1 = new Categoria(null, "Teste", "qualquer coisa");
-        Categoria c2 = new Categoria(null, "livro 2", "outro negocio");
-        Livro l1 = new Livro(null, "qualquer coisa", "fulano", "nada",
+        Categoria c1 = new Categoria(null, "Tecnologia", "Livros da área de tecnologia");
+        Categoria c2 = new Categoria(null, "Financeiro", "Livros sobre finanças");
+        Livro l1 = new Livro(null, "Angular 10", "fulano 1 ", "Essencial na vida de um dev",
                 LocalDate.of(2021,06,22),c1);
-        Livro l2 = new Livro(null, "qualquer bbbbbbb  coisa", "fulano 2 novo", "nadsdsadsada",
-                LocalDate.of(2021,06,22),c1);
-        Livro l3 = new Livro(null, "novo livro", "fulano 3", "naaaaaaaaaaaaaaada",
+        Livro l2 = new Livro(null, "Pai Rico, Pai Pobre", "fulano 2", "Para a vida",
                 LocalDate.of(2021,06,22),c2);
+        Livro l3 = new Livro(null, "JAVA", "Paul Deitel", "Como ser feliz...",
+                LocalDate.of(2021,06,22),c1);
 
         categoriaRepositorio.saveAll(Arrays.asList(c1,c2));
         livroRepositorio.saveAll(Arrays.asList(l1,l2,l3));
